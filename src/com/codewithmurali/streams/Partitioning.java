6591a2e59df8d941bb93976beb8041a5a4f7a4f6
Murali Routhu <muralikrishna.routhu@zemosolabs.com>
2022-03-09 13:08:20 +0530

    Updated to partitioning in streams.

---
+++ src/com/codewithmurali/streams/Partitioning.java
@@ -0,0 +1,17 @@
+package com.codewithmurali.streams;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Partitioning {
+    public static void main(String[] args) {
+        List<MovieWithGenre> movies = List.of(
+                new MovieWithGenre("a",10,Genre.THRILLER),
+                new MovieWithGenre("b",20,Genre.ACTION),
+                new MovieWithGenre("c",2,Genre.ACTION)
+        );
+        System.out.println(movies.stream().collect(Collectors.partitioningBy(m->m.getLikes()>5,
+                Collectors.mapping(MovieWithGenre::getTitle,Collectors.joining(", ")))));
+
+    }
+}
